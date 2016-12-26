fc0acd6722b604078199dfe54ed62d1c5e08c3b2
saiga12 <aiasfina@hotmail.com>
2016-12-27 06:34:27 +0800

    Fixed missing libapparmor.so.1

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ case "$1" in
   	;;
   	run)	
     	docker run -d -v /var/run/docker.sock:/var/run/docker.sock \
+		   -v /lib/x86_64-linux-gnu/libapparmor.so.1:/lib/x86_64-linux-gnu/libapparmor.so.1 \
                	   -v $(which docker):/usr/bin/docker \
                    -p 8080:8080 --name $NAME $NAME
   	;;
